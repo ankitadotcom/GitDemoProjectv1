4ff2d55982130a5f4b868196e1bc5603015e6456
Ankita Vyas <ankita0439@gmail.com>
2022-07-30 13:05:22 +0530

    age varible to check user age is eligible for vote or not vote...

--- src/sumdemo1.java
+++ src/sumdemo1.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class sumdemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		System.out.print("Enter Number 1=");
-		int n1=s.nextInt();
+		int num1=s.nextInt();
 		System.out.print("Enter Number 2=");
-		int n2=s.nextInt();
-		int sum=n1+n2;
+		int num2=s.nextInt();
+		int sum=num1 + num2;
 		System.out.println("Sum is="+sum);
-
 	}
 
 }
